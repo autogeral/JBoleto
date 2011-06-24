b5f3d4dc6d4953ab799b0b695316017ac4b3b4ac
murilolima <murilolima@5adba155-f38a-43c0-aea0-d88c706dde45>
2011-06-24 18:04:06 +0000

    [add] exemplo de boleto do Bradesco para Empresa Marton
    git-svn-id: https://svn.autogeral.com.br/code/JBoleto@487 5adba155-f38a-43c0-aea0-d88c706dde45

--- test/org/jboleto/exemplos/ExemploBradesco.java
+++ test/org/jboleto/exemplos/ExemploBradesco.java
@@ -9,8 +9,6 @@
  * Veja a Licenca Publica Geral Reduzida GNU para maiores detalhes. 
  * A licenca se encontra no arquivo lgpl-br.txt 
  */
-
-
 package org.jboleto.exemplos;
 
 import java.util.Vector;
@@ -21,54 +19,52 @@ import org.jboleto.JBoletoBean;
  * @author Fabio Souza
  */
 class ExemploBradesco {
-        
-	public static void main(String args[]) {
-        
-		JBoletoBean jBoletoBean = new JBoletoBean();
-		
-		jBoletoBean.setDataDocumento("31/05/2006");
-        jBoletoBean.setDataProcessamento("31/05/2006");      
-            
-        jBoletoBean.setCedente("AINODE Solucoes");  
-        jBoletoBean.setCarteira("06");
 
-        jBoletoBean.setNomeSacado("GtTurbo");
-        jBoletoBean.setEnderecoSacado("Rua Araticum 951");        
-        jBoletoBean.setBairroSacado("Anil");
-        jBoletoBean.setCidadeSacado("Rio de Janeiro");
-        jBoletoBean.setUfSacado("RJ");
-        jBoletoBean.setCepSacado("22753-501");
-        jBoletoBean.setCpfSacado("87524988753");
-        
-        jBoletoBean.setLocalPagamento("ATE O VENCIMENTO, PREFERENCIALMENTE NO BRADESCO");
-        jBoletoBean.setLocalPagamento2("APOS O VENCIMENTO, SOMENTE NO BRADESCO");        
-        
+    public static void main(String args[]) {
+
+        JBoletoBean jBoletoBean = new JBoletoBean();
+
+        jBoletoBean.setDataDocumento("20/06/2011");
+        jBoletoBean.setDataProcessamento("20/06/2011");
+
+        jBoletoBean.setCedente("Bruna Cartuchos e Suprimentos para Impressoras Ltda");
+        jBoletoBean.setCarteira("09");
+
+        jBoletoBean.setNomeSacado("MURILO SOARES LIMA");
+        jBoletoBean.setEnderecoSacado("RUA PORTUGAL, 205");
+        jBoletoBean.setBairroSacado("VILA ROMA");
+        jBoletoBean.setCidadeSacado("ITU");
+        jBoletoBean.setUfSacado("SP");
+        jBoletoBean.setCepSacado("13310-440");
+        jBoletoBean.setCpfSacado("359.821.558-45");
+
+        jBoletoBean.setLocalPagamento("Pagável Preferencialmente na rede Bradesco ou Banco Postal");
+
         Vector descricoes = new Vector();
-        descricoes.add("Hospedagem I - teste descricao1 - R$ 39,90");
-        descricoes.add("Manutencao - teste ricao2 - R$ 32,90");
-        descricoes.add("Sistema - teste ssssde descricao3 - R$ 45,90");
-        descricoes.add("Extra - teste de descricao4 - R$ 78,90");
+        descricoes.add("Teste");
         jBoletoBean.setDescricoes(descricoes);
-        
-        jBoletoBean.setDataVencimento("10/06/2006");
-        jBoletoBean.setInstrucao1("APOS O VENCIMENTO COBRAR MULTA DE 2%");
-        jBoletoBean.setInstrucao2("APOS O VENCIMENTO COBRAR R$ 0,50 POR DIA DE ATRASO");
+
+        jBoletoBean.setDataVencimento("20/07/2011");
+        jBoletoBean.setInstrucao1("APOS O VENCIMENTO COBRAR R$ 0,50 POR DIA DE ATRASO");
         jBoletoBean.setInstrucao3("");
         jBoletoBean.setInstrucao4("");
 
-        jBoletoBean.setAgencia("2949");
-        jBoletoBean.setDvAgencia("1");
-        
-        jBoletoBean.setContaCorrente("0006580");
-        jBoletoBean.setDvContaCorrente("3");
-        
-        jBoletoBean.setNossoNumero("003",11);        
+        jBoletoBean.setAgencia("1724");
+        jBoletoBean.setDvAgencia("8");
+        jBoletoBean.setAceite("N");
+        jBoletoBean.setEspecieDocumento("DM");
+
+        jBoletoBean.setContaCorrente("5455");
+        jBoletoBean.setDvContaCorrente("0");
+
+        jBoletoBean.setNossoNumero("1", 11);
+        jBoletoBean.setNoDocumento("1");
         jBoletoBean.setValorBoleto("1.00");
-        
+
         JBoleto jBoleto = new JBoleto();
-        
-        jBoleto.addBoleto(jBoletoBean,JBoleto.BRADESCO);
-        jBoleto.writeToFile("bradesco.pdf");	        
-            
-	}
+
+        jBoleto.addBoleto(jBoletoBean, JBoleto.BRADESCO);
+        jBoleto.writeToFile("bradesco.pdf");
+
+    }
 }
