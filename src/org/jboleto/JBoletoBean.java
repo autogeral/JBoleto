544935a1d499324ff46175fbdb6a270f7da39049
murilolima <murilolima@5adba155-f38a-43c0-aea0-d88c706dde45>
2011-07-26 12:19:29 +0000

    [update] metodo para deixar nome do cliente somente com a primeira letra em maiusculo
    git-svn-id: https://svn.autogeral.com.br/code/JBoleto@665 5adba155-f38a-43c0-aea0-d88c706dde45

--- src/org/jboleto/JBoletoBean.java
+++ src/org/jboleto/JBoletoBean.java
@@ -93,6 +93,39 @@ public class JBoletoBean {
 
         return diferencaDias;
     }
+    
+    public static String soPrimeiraMaiuscula (String astring) {
+        if(astring==null){
+            return null;
+        }
+        String palavras [] = astring.split(" ");
+        StringBuilder sb = new StringBuilder();
+        boolean primeira = true;
+        for(String palavra : palavras) {
+            if(primeira) {
+                primeira = false;
+            } else {
+                sb.append(" ");
+            }
+            String tratada = soPrimeiraMaiusculaPalavra(palavra);
+            sb.append(tratada);
+        }
+        return sb.toString();
+    }
+    
+    public static String soPrimeiraMaiusculaPalavra (String astring) {
+        if(astring==null) {
+            return null;
+        }
+        String string = astring.toLowerCase();
+        if(string.length()==1) {
+            return string;
+        }
+        if(string.equals("da") || string.equals("de") || string.equals("do")) {
+            return string;
+        }
+        return (string.length()>2?string.substring(0, 1).toUpperCase()+string.substring(1):string);
+    }
 
     /**
      * Retorna o numero da agencia.
