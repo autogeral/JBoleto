dbe1968fe89b44e5ea17ecab094471b4464560be
Larissa-Antunes <larissa.antunes12357@gmail.com>
2021-08-23 15:15:49 -0300

    [add]criando um exemplo do boleto itaú em pdf.

--- test/org/jboleto/exemplos/ExemploItau.java
+++ test/org/jboleto/exemplos/ExemploItau.java
@@ -1,16 +1,7 @@
-/*
- * Esta biblioteca e um software livre, que pode ser redistribuido e/ou 
- * modificado sob os termos da LicenÃ§a Publica Geral Reduzida GNU, 
- * conforme publicada pela Free Software Foundation, versao 2.1 da licenca.
- *
- * Esta biblioteca e distribuida na experanca de ser util aos seus usuarios, 
- * porem NAO TEM NENHUMA GARANTIA, EXPLICITAS OU IMPLICITAS, COMERCIAIS OU 
- * DE ATENDIMENTO A UMA DETERMINADA FINALIDADE. 
- * Veja a Licenca Publica Geral Reduzida GNU para maiores detalhes. 
- * A licenca se encontra no arquivo lgpl-br.txt 
- */
+
 package org.jboleto.exemplos;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Vector;
 import java.util.logging.Level;
@@ -20,44 +11,42 @@ import org.jboleto.JBoletoBean;
 
 /**
  * @author Fabio Souza
+ *
  */
 class ExemploItau {
 
     public static void main(String args[]) {
-        testaJ();
-//        testaPatricia();
-    }
-
-    private static void testaJ() {
         JBoletoBean jBoletoBean = new JBoletoBean();
+        jBoletoBean.setDataDocumento("23/08/2021");
+        jBoletoBean.setDataProcessamento("23/08/2021");
 
-        jBoletoBean.setDataDocumento("01/01/2010");
-        jBoletoBean.setDataProcessamento("01/01/2010");
+        jBoletoBean.setAceite("N");
+        jBoletoBean.setEspecieDocumento("DM");
+        jBoletoBean.setCedente("AUTO GERAL DE ITU LTDA");
+        jBoletoBean.setCarteira("109");
 
-        jBoletoBean.setCedente("J. Computação LTDA - ME");
-        //jBoletoBean.setCarteira("198");
-        jBoletoBean.setCarteira("110");
-
-        jBoletoBean.setNomeSacado("Patricia Alvarenga Gontijo de Souza");
-        jBoletoBean.setEnderecoSacado("Av. Dom Pedro II, 348");
-        jBoletoBean.setBairroSacado("Centro");
+        jBoletoBean.setNomeSacado("Larissa Antunes de Camargo");
+        jBoletoBean.setEnderecoSacado("Rua San Marino, 323");
+        jBoletoBean.setBairroSacado("Jd Saltense");
         jBoletoBean.setCidadeSacado("Salto");
         jBoletoBean.setUfSacado("SP");
-        jBoletoBean.setCepSacado("13320-240");
-        jBoletoBean.setCpfSacado("049.747.666-51");
+        jBoletoBean.setCepSacado("13327-271");
+        jBoletoBean.setCpfSacado("500.683.354-64");
+
+        jBoletoBean.setLocalPagamento("ATE O VENCIMENTO, PREFERENCIALMENTE NO ITAÚ");
+        jBoletoBean.setLocalPagamento2("APOS O VENCIMENTO, SOMENTE NO ITAÚ");
 
         Vector descricoes = new Vector();
         descricoes.add("Boleto de teste para homologação");
-
         jBoletoBean.setDescricoes(descricoes);
 
-//        jBoletoBean.setImagemMarketing("/home/fabio/template_logo.png");
-        jBoletoBean.setDataVencimento("01/05/2002");
+        jBoletoBean.setDataVencimento("01/09/2021");
         jBoletoBean.setInstrucao1("APOS O VENCIMENTO COBRAR MULTA DE 2%");
         jBoletoBean.setInstrucao2("APOS O VENCIMENTO COBRAR R$ 0,50 POR DIA DE ATRASO");
         jBoletoBean.setInstrucao3("");
         jBoletoBean.setInstrucao4("");
 
+        
         jBoletoBean.setAgencia("0057");
         jBoletoBean.setContaCorrente("12345");
         jBoletoBean.setDvContaCorrente("7");
@@ -69,61 +58,23 @@ class ExemploItau {
         jBoletoBean.setNoDocumento("0000001");
         jBoletoBean.setValorBoleto("123.45");
 
-        JBoleto jBoleto = new JBoleto();
-
-        jBoleto.addBoleto(jBoletoBean, JBoleto.ITAU);
-        try {
-            jBoleto.writeToFile("testeManualImplementacaoItau.pdf");
-        } catch (IOException ex) {
-            Logger.getLogger(ExemploItau.class.getName()).log(Level.SEVERE, null, ex);
+        
+        String valor = jBoletoBean.getModulo11("3419204600000273719028203356661245780020102", 2);
+        if(valor.equals("6")){
+            System.out.println("Dv do codigo de barras esta correto");
+        }else{
+            System.out.println("Dv do codigo de barras NAO esta correto");
         }
-    }
-
-    private static void testaPatricia() {
-        JBoletoBean jBoletoBean = new JBoletoBean();
-
-        jBoletoBean.setDataDocumento("01/01/2010");
-        jBoletoBean.setDataProcessamento("01/01/2010");
-
-        jBoletoBean.setCedente("Patricia Alvarenga Gontijo de Souza");
-        jBoletoBean.setCarteira("198");
-
-        jBoletoBean.setNomeSacado("Murilo de Moraes Tuvani");
-        jBoletoBean.setEnderecoSacado("Rua Marechal Deodoro, 505 ap 132");
-        jBoletoBean.setBairroSacado("Chafariz");
-        jBoletoBean.setCidadeSacado("Itu");
-        jBoletoBean.setUfSacado("SP");
-        jBoletoBean.setCepSacado("13300-110");
-        jBoletoBean.setCpfSacado("279.339.928-03");
-
-        Vector<String> descricoes = new Vector<String>();
-        descricoes.add("Boleto de teste para homologação");
-
-        jBoletoBean.setDescricoes(descricoes);
-
-//        jBoletoBean.setImagemMarketing("/home/fabio/template_logo.png");
-        jBoletoBean.setDataVencimento("31/08/2010");
-        jBoletoBean.setInstrucao1("APOS O VENCIMENTO COBRAR MULTA DE 2%");
-        jBoletoBean.setInstrucao2("APOS O VENCIMENTO COBRAR R$ 0,50 POR DIA DE ATRASO");
-        jBoletoBean.setInstrucao3("");
-        jBoletoBean.setInstrucao4("");
-
-        jBoletoBean.setAgencia("8162");
-        jBoletoBean.setContaCorrente("02872");
-        jBoletoBean.setDvContaCorrente("7");
-        //Documentacao especifica 5 numeros
-        jBoletoBean.setCodCliente("00000");
-
-        jBoletoBean.setNossoNumero("1", 8);
-        //especificacao para carteira 198 sao 7 numeros
-        jBoletoBean.setNoDocumento("0000001");
-        jBoletoBean.setValorBoleto("2.00");
-
+        
         JBoleto jBoleto = new JBoleto();
+        jBoleto.addBoleto(jBoletoBean, JBoleto.ITAU);      
 
-        jBoleto.addBoleto(jBoletoBean, JBoleto.ITAU);
+        File file = new File("boletoItauTeste.pdf");
+        
         try {
-            jBoleto.writeToFile("itauPatricia.pdf");
+            jBoleto.writeToFile(file);
+            java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+            desktop.browse(file.toURI());
         } catch (IOException ex) {
             Logger.getLogger(ExemploItau.class.getName()).log(Level.SEVERE, null, ex);
         }
